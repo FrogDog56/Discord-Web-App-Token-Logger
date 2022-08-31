e36a0914c05e11d4394323fec0f3247846a64858
FrogDog56 <83395400+FrogDog56@users.noreply.github.com>
2022-08-31 22:21:43 +0100

    first working version

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import selenium
 import warnings
 import os
 import time
+import pyperclip
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
@@ -15,6 +16,7 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 option = webdriver.ChromeOptions()
 option.add_argument("--mute-audio")
+option.add_extension(os.getcwd() + "\solver.crx")
 option.add_experimental_option("excludeSwitches", ["enable-logging"])
 
 driver = webdriver.Chrome(ChromeDriverManager().install(), options = option)
@@ -24,5 +26,14 @@ time.sleep(2)
 
 driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/div/div[1]/div/div/div/div/form/div/div/div[1]/div[2]/div[1]/div/div[2]/input").send_keys(user)
 driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/div/div[1]/div/div/div/div/form/div/div/div[1]/div[2]/div[2]/div/input").send_keys(password)
+
+time.sleep(1)
+
+driver.find_element(By.XPATH, "//*[@id='app-mount']/div[2]/div/div[1]/div/div/div/div/form/div/div/div[1]/div[2]/button[2]").click()
+
+time.sleep(60)
+
+token = driver.execute_script("""return (webpackChunkdiscord_app.push([[''],{},e=>{m=[];for(let c in e.c)m.push(e.c[c])}]),m).find(m=>m?.exports?.default?.getToken!==void 0).exports.default.getToken()""")
+print(token)
 
 time.sleep(360)
